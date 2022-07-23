1c3adadc74dccaf28d2f1d5e87ff08ff409fc01d
Diana Murtazina <murka8831@yandex.ru>
2022-07-23 18:08:50 +0400

    working with fetch

---
+++ src/script2.js
@@ -0,0 +1,35 @@
+let searchForm = document.querySelector("#search-form");
+let movie = document.querySelector("#movies");
+
+function apiSearch(event) {
+    event.preventDefault();
+    let searchText = document.querySelector(".form-control").value;
+    let server = `https://api.themoviedb.org/3/search/multi?api_key=a11a1a6a5a534565f99c09241f77ff27&language=ru&query=${searchText}`;
+    movie.innerHTML = "Загрузка";
+
+    fetch(server)
+        .then(function (value) {
+            if (value.status !== 200) {
+                return Promise.reject(new Error(value.status));
+            }
+            return value.json();
+        })
+        .then(function (output) {
+            let inner = "";
+
+            output.results.forEach(function (item) {
+                let nameItem = item.name || item.title;
+                let releaseDate = item.release_date || item.first_air_date;
+
+                inner += `<div class='col-12 col-md-4 col-xl-3'>${nameItem} <div> Дата выхода: ${releaseDate} </div></div>`;
+            });
+
+            movie.innerHTML = inner;
+        })
+        .catch(function (reason) {
+            movie.innerHTML = "Ooops, что-то пошло не так!";
+            console.log("error: " + reason);
+        });
+}
+
+searchForm.addEventListener("submit", apiSearch);
