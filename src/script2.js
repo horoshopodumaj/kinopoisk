5978a205e406ed3254784ecf78509c39e31f48ac
Diana Murtazina <murka8831@yandex.ru>
2022-07-24 17:15:04 +0400

    trends for the week have been added to the main page

--- src/script2.js
+++ src/script2.js
@@ -66,3 +66,46 @@ function addEventMedia() {
 function showFullInfo() {
     console.log(this);
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch(
+        "https://api.themoviedb.org/3/trending/all/week?api_key=a11a1a6a5a534565f99c09241f77ff27&language=ru"
+    )
+        .then(function (value) {
+            if (value.status !== 200) {
+                return Promise.reject(new Error(value.status));
+            }
+            return value.json();
+        })
+        .then(function (output) {
+            let inner = `<h4 class='col-12 text-center text-info item'>Популярное за неделю</h2>`;
+            if (output.results.length === 0) {
+                inner = `<h2 class='col-12 text-center text-info item'>К сожалению, ничего не найдено</h4>`;
+            }
+            output.results.forEach(function (item) {
+                let nameItem = item.name || item.title;
+                let releaseDate = item.release_date || item.first_air_date;
+                let poster = item.poster_path
+                    ? urlPoster + item.poster_path
+                    : "./img/no_poster.jpg";
+
+                let dataInfo = "";
+                if (item.media_type !== "person")
+                    dataInfo = `data-id = ${item.id} data-type=${item.media_type}`;
+                inner += `
+                <div class='col-12 col-md-6 col-xl-3 item'>
+                <img src=${poster} class='img_poster' alt='${nameItem}' ${dataInfo}>
+                    <h5>${nameItem}</h5>
+                    <div>Дата выхода: ${releaseDate} </div>
+                </div>`;
+            });
+
+            movie.innerHTML = inner;
+
+            addEventMedia();
+        })
+        .catch(function (reason) {
+            movie.innerHTML = "Ooops, что-то пошло не так!";
+            console.log("error: " + reason);
+        });
+});
